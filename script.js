9ea24368e7c1443286bfed098a61e2ccb24c6051
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-03-08 15:41:03 +0200

    added functionality to change bkgrd color when hovering over divs

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// const container = document.querySelector("#container");
-// const grid = document.querySelector(".grid");
-// const squares = document.createElement("div");
-// squares.classList.add("square");
-// squares.setAttribute(
-//   "style",
-//   "border: solid 2px; max-width: 50px; height: 50px"
-// );
-// grid.appendChild(squares);
-
-// grid.setAttribute(
-//   "style",
-//   "border: 5mm ridge rgba(83, 105, 172, .8); max-width: 50rem; height: 50rem"
-// );
-
-// squares.addEventListener("click", addColor);
-
-// function addColor() {
-//   squares.setAttribute(
-//     "style",
-//     "border: solid 2px; max-width: 50px; height: 50px; background-color: black"
-//   );
-// }
-
 const container = document.getElementById("grid-container");
 const resetBtn = document.getElementById("reset");
 
@@ -29,6 +5,10 @@ function createGrid() {
   for (let i = 0; i < 256; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid-item");
+
+    grid.addEventListener("mouseover", function () {
+      grid.setAttribute("style", "background-color:red; transition:0.4s");
+    });
     container.appendChild(grid);
   }
 }
